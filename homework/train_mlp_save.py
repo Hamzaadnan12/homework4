8da7418e31170b686b3d8b2024c714c0fb775a05
Hamza Adnan <hamzaadnan@MacBook-Pro.local>
2025-04-20 21:35:50 -0400

    test

--- homework/train_mlp_save.py
+++ homework/train_mlp_save.py
@@ -39,8 +39,8 @@ def train(
     model.train()
 
     # Load data
-    train_data = load_data("/drive_data/train", shuffle=True, batch_size=batch_size, num_workers=2)
-    val_data = load_data("/drive_data/val", shuffle=False, batch_size=batch_size, num_workers=2)
+    train_data = load_data("drive_data/train", shuffle=True, batch_size=batch_size, num_workers=2)
+    val_data = load_data("drive_data/val", shuffle=False, batch_size=batch_size, num_workers=2)
 
     loss_func = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
